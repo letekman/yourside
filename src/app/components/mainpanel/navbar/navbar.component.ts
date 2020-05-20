8047cf4955948c118353d4ef9e0b6a6896894408
pyreq <pyreq@users.noreply.github.com>
2020-05-20 14:03:56 +0200

    fixed sidebar not opening
    buttons except "sign out" are not present

--- src/app/components/mainpanel/navbar/navbar.component.ts
+++ src/app/components/mainpanel/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  private toggleButton: any;
+  private sidebarVisible: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private element: ElementRef) {
+    this.sidebarVisible = false;
   }
 
+  ngOnInit() {
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+  }
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const html = document.getElementsByTagName('html')[0];
+    setTimeout(function() {
+      toggleButton.classList.add('toggled');
+    }, 500);
+    html.classList.add('nav-open');
+
+    this.sidebarVisible = true;
+  };
+  sidebarClose() {
+    const html = document.getElementsByTagName('html')[0];
+    // console.log(html);
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    html.classList.remove('nav-open');
+  };
+  sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
+    }
+  };
 }
