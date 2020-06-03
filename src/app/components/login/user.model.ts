56b274dd32518c90f5eadda08464ededa9cd64cf
Olha <8unicorn89@gmail.com>
2020-06-03 17:25:30 +0200

    changes with getting roles

--- src/app/components/login/user.model.ts
+++ src/app/components/login/user.model.ts
@@ -1,10 +1,10 @@
 export class UserModel {
     id: number;
     username: string;
-    roles: object[];
-    constructor(id: number, username: string, roles: object[]) {
+    role: object;
+    constructor(id: number, username: string, roles: object) {
         this.id = id;
         this.username = username;
-        this.roles = roles;
+        this.role = roles;
     }
 }
